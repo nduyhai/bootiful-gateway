d05fe0945b9f8828f84cd5edbad59c517e2ebcbe
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 19:53:47 +0700

    init project

---
+++ src/main/java/com/bootiful/gateway/BootifulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bootiful.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootifulGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bootiful/gateway/BootifulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootiful.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
